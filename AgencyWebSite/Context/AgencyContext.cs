e69a6301ce509ae54588087d29e409d987d0913f
Mehmet Polat <mehwetpolat@gmail.com>
2025-02-18 00:01:41 +0300

    version admin login phase

--- AgencyWebSite/Context/AgencyContext.cs
+++ AgencyWebSite/Context/AgencyContext.cs
@@ -16,5 +16,11 @@ namespace AgencyWebSite.Context
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectDetail> ProjectDetails { get; set; }
         public DbSet<Team> Teams { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Branch> Branchs { get; set; }
+        
+        
     }
 }
